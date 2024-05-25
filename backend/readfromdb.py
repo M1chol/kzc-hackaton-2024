9305c30353c6e049fac3f9fac76fad09f31022fa
Fred <64082311+MatiP911@users.noreply.github.com>
2024-05-25 15:41:36 +0200

    misic again

--- backend/readfromdb.py
+++ backend/readfromdb.py
@@ -14,10 +14,11 @@ class Element():
         self.date = str(datetime.datetime.now());
         self.experimentationDate = '';
         self.iconID = 0;
+        self.like = 0;
     
     def UpdateParam(self, **kwargs):
         for paramName, newVal in kwargs.items():
-            if hasattr(self, paramName):
+            if str(paramName) != 'ID' and hasattr(self, paramName):
                 setattr(self, paramName, newVal)
             else:
                 raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{paramName}'")
@@ -29,11 +30,17 @@ class DBHandling():
         self.db.insert({'ID': self.__lastID()+1, 'x': Element.x, 'y': Element.y, 
                         'txt': Element.txt, 'authorID': Element.authorID,
                         'date': Element.date, 'iconID': Element.iconID, 
-                        'experimentationDate': Element.experimentationDate})
+                        'experimentationDate': Element.experimentationDate,
+                        'like': 0})
     
     def getAll(self) -> list:
         return self.db.all()
     
+    # def likePost(self, ID:int):
+    #     database = Query()
+    #     like=self.db.search(database.ID == ID)[0]
+    #     self.db.update({'like': like+1}, database.ID == ID)
+
     def __clearData(self) -> None:
         self.db.truncate()
     
@@ -47,7 +54,8 @@ class DBHandling():
 if __name__ == '__main__':
     DBHandler = DBHandling()
     ele1 = Element("twoja stara")
-    ele1.UpdateParam(ID = 4, x=130, y=15)
+    ele1.UpdateParam(x=130, y=15)
     DBHandler.addEle(ele1)
+    # DBHandler.likePost(4)
 
     print(DBHandler.getAll())
